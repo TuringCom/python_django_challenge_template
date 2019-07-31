c905298ebead4037896cac8bad248ff9078b3ffd
Malisa Ncube <malisa.ncube@gmail.com>
2019-08-01 00:11:44 +0300

    Fix path

--- src/turing_backend/urls.py
+++ src/turing_backend/urls.py
@@ -36,5 +36,5 @@ urlpatterns = [
     url(r'^docs(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api/', include('api.urls'))
+    path('', include('api.urls'))
 ]
